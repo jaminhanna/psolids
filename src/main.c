a25aa8bdbff9b803dfd9c4df2e1a329e4512ec11
Jamin Hanna <jaminmckenziehanna@protonmail.com>
2024-03-04 02:43:16 -0500

    Completing working makefile

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     case 1  : name = "stdin"; stream = stdin;                 break;
     default : name = argv[1]; stream = fopen(argv[1],   "r"); break;
   }
-  for (i = 1; stream != NULL; stream = fopen(argv[i++], "r")) {
+  for (i = 2; stream != NULL; stream = fopen(argv[i++], "r")) {
     if ((len = getword()) == 0) { /* no word to read */
       if (i == argc) return EXIT_SUCCESS; /* no more files */
       continue; /* ignore empty files */
